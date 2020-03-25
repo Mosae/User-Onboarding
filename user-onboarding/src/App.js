90c1aa02e428304cbeb8c185f699b9626954eac5
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-25 18:44:10 -0500

    started step 1 of project

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Form from './Form.js';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">Hello there</header>
+			<div className="App-header">
+				<h1>User Onboarding </h1>
+
+				<Form />
+			</div>
 		</div>
 	);
 }
---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Form() {
+	return (
+		<form>
+			<label htmlFor="name">
+				Name:
+				<input id="name" />
+			</label>
+		</form>
+	);
+}
+
+export default Form;
