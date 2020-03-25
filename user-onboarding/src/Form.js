0926e90af6803a847355973917a8aef38b3cc05b
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-25 18:52:45 -0500

    created 3 input fields in form.js

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -5,8 +5,21 @@ function Form() {
 		<form>
 			<label htmlFor="name">
 				Name:
-				<input id="name" />
+				<input id="name" type="text" name="name" />
 			</label>
+			<br />
+
+			<label htmlFor="email">
+				Email:
+				<input id="email" type="text" name="email" />
+			</label>
+			<br />
+
+			<label htmlFor="password">
+				Password:
+				<input type="text" name="password" />
+			</label>
+			<br />
 		</form>
 	);
 }
