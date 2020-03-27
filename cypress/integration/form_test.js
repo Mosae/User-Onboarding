aad4136d946429843452f248e61d6dd953c14670
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-26 23:43:12 -0500

    Step 2 Name input passes

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,5 +1,11 @@
 describe('Testing our form inputs', function() {
-	it('visits our local host', function() {
+	beforeEach(function() {
 		cy.visit('http://localhost:3000/');
+	});
+	it('Adds text to inputs and submits form', function() {
+		cy.get('input[name="name"]')
+			.type('Mosae')
+			.should('have.value', 'Mosae');
+		cy.get('input ');
 	});
 });
